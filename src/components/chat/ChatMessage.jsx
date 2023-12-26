aeb57e2f94331166d67dd709cfb4a792f0e2e2a6
Sean <91260510+seanwhitee@users.noreply.github.com>
2023-12-26 15:54:27 +0800

    Add react-syntax-highlighter for code highlighting

--- src/components/chat/ChatMessage.jsx
+++ src/components/chat/ChatMessage.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import Image from "next/image";
 import remarkGfm from "remark-gfm";
 import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const ChatMessage = ({ message }) => {
   return message?.user.name === "ChatGpt" ||
@@ -22,6 +24,23 @@ const ChatMessage = ({ message }) => {
 
       <div className="border-2 rounded-lg border-white/50 p-5 w-10/12 font-light text-black">
         <Markdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  style={atomDark}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props} />
+              );
+            },
+          }}
           className="flex flex-col overflow-scroll lg:text-sm md:text-sm text-xs"
           remarkPlugins={[remarkGfm]}
         >
